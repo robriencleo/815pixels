8704a8565bef9cfd6b74aa8f58b612ca47db7e12
Rebecca O'Brien <robrien@cleo.com>
2019-11-11 15:27:02 -0600

    Update hc-external.js

--- hc-external.js
+++ hc-external.js
@@ -1,9 +1,9 @@
 jQuery(function($) {
   $('#CICControl td').click(function() {
-	var ControlID = '#' + $(this).attr('id') + 'Output';
+  var ControlID = '#' + $(this).attr('id') + 'Output';
   $('#CICControl td').removeClass('selected');
   $(this).addClass('selected');
-	$('#CICOutput div').hide();
-	$(ControlID).show();
+  $('.CICOutput').hide();
+  $(ControlID).show();
   });
 });
